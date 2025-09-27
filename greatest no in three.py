cffaf5860e324dfba76dfad55d099d413ed88952
Imraan0012 <192424001.simats@saveetha.com>
2025-09-27 08:19:08 +0530

    Add files via upload

---
+++ greatest no in three.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the first no "))
+b=int(input("Enter the second no "))
+c=int(input("Enter the Third no "))
+if a>b:
+    if a>c:
+        print("the greatest no is ",a)
+    else:
+        print("the greatest no is ",c)
+else b>c:
+    if b>c:
+        print("the greatest number is ",b)
+    else:
+        print("the greatest number is ",c)
